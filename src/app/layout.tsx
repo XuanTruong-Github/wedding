61f5ed50fdb96a289bc76789093209c6bcfd9bf5
XuanTruong-Github <lexuantruong.contacts@gmail.com>
2024-01-04 17:28:26 +0700

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Comfortaa } from "next/font/google";
 import "@/styles/globals.css";
 
@@ -5,6 +6,25 @@ const font = Comfortaa({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
+
+export const metadata: Metadata = {
+  title: "Xuân Trường & Tú Anh",
+  description:
+    "Lê Xuân Trường & Bùi Tú Anh - Ngày cưới: 2024-01-14 |  Yêu còn hơn mê. Đó là nhu cầu được cảm thấy toàn bộ, cảm thấy an toàn, được chữa lành, kết hợp với một ai đó, trái tim và linh hồn",
+  keywords: ["Lê Xuân Trường", "Bùi Tú Anh", "Xuân Trường", "Tú Anh"],
+  openGraph: {
+    title: "Xuân Trường & Tú Anh",
+    url: "https://wedding-beryl-beta.vercel.app",
+    description:
+      "Lê Xuân Trường & Bùi Tú Anh - Ngày cưới: 2024-01-14 | Yêu còn hơn mê. Đó là nhu cầu được cảm thấy toàn bộ, cảm thấy an toàn, được chữa lành, kết hợp với một ai đó, trái tim và linh hồn",
+    type: "website",
+    images: [
+      "https://osrokyoftlwfyhywgwui.supabase.co/storage/v1/object/public/wedding/other/thank-you.jpg",
+    ],
+  },
+  creator: "Xuân Trường",
+  metadataBase: new URL("https://wedding-beryl-beta.vercel.app"),
+};
 
 export default function RootLayout({
   children,
