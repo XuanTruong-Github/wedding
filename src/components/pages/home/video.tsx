9558ea5c7bf4eb8c1fff4eff76efa5316f04a94a
XuanTruong-Github <lexuantruong.contacts@gmail.com>
2024-01-07 14:35:00 +0700

    updated

--- src/components/pages/home/video.tsx
+++ src/components/pages/home/video.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
@@ -24,7 +25,10 @@ export default function Video({ image }: Props) {
       <div className="absolute top-0 left-0 h-full w-full z-[1] grid place-items-center bg-black/20">
         <div className="text-white text-center container">
           <h1
-            className={cn(font.className, "text-4xl lg:text-6xl mb-4 text-white")}
+            className={cn(
+              font.className,
+              "text-4xl lg:text-6xl mb-4 text-white"
+            )}
             style={{ textShadow: "1px 1px 2px rgba(0, 0, 0, 0.6)" }}
           >
             Xem video cưới của chúng tôi
@@ -34,12 +38,9 @@ export default function Video({ image }: Props) {
           </p>
           <Dialog>
             <DialogTrigger>
-              <Button
-                size={"icon"}
-                className="rounded-full bg-white/90 h-12 w-12"
-              >
+              <div className="rounded-full bg-white/90 h-12 w-12 grid place-items-center">
                 <Play className="text-primary" />
-              </Button>
+              </div>
             </DialogTrigger>
             <DialogContent>
               <iframe
